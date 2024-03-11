9bd9fef751e89799a29874d55855dcbfa7af27cd
Loa Gnadjou Soumahoro <loagnadjou@gmail.com>
2024-03-11 15:36:48 +0100

    Ajout de l'event click pour l'envoie du nombre saisir dans l'input

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// let message = (document.querySelector('.message').textContent =
+//   '🎉 Correct Number');
+// console.log(message);
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// const inputGuess = document.querySelector('.guess ');
+// inputGuess.value = 23;
+
+const buttonCheck = document.querySelector('.check');
+//Le type de l'input est en string ila faut le convertire en nombre.
+const inputGuess = Number(document.querySelector('.guess').value);
+
+buttonCheck.addEventListener('click', function () {
+  console.log(inputGuess, typeof inputGuess);
+});
